6aeee1813a7644d1605a10aaf0cc4f675d353b70
samhimes92 <64994860+samhimes92@users.noreply.github.com>
2022-11-11 16:03:26 -0700

    Add files via upload
    update r

--- analyze_raw_input_data.R
+++ analyze_raw_input_data.R
@@ -1,32 +1,62 @@
-raw_csv <- read_csv("~/Desktop/U_of_U/Fall_2022/data_vis/final_project/DataVis-Project/input_data/songs_5_500.csv")
+library(tidyverse)
+library(dplyr)
+library(jsonlite)
 
-raw_csv %>% 
+
+setwd("/Users/sam/Desktop/U_of_U/Fall_2022/data_vis/final_project/DataVis-Project/source_code/vis_data")
+
+#read all of the csv's
+raw_csv1 <- read_csv("~/Desktop/U_of_U/Fall_2022/data_vis/final_project/DataVis-Project/input_data/songs_5_500.csv")
+raw_csv2 <- read_csv("~/Desktop/U_of_U/Fall_2022/data_vis/final_project/DataVis-Project/input_data/songs_0_5.csv")
+raw_csv3 <- read_delim("~/Desktop/U_of_U/Fall_2022/data_vis/final_project/DataVis-Project/input_data/songs_131_135.csv", delim = "|")
+
+#combine all the csv's
+all_songs <- bind_rows(raw_csv1, raw_csv2) %>% 
+  filter(totalWords < 5000) #Filter because some songs seem to have returned paragraph written about the song, not the lyrics
+
+#Work with the most common words (skip for now)
+all_songs %>% select(genre, year, contains("cw")) %>%
+  group_by(cw1, cw2, cw3,cw4,cw5,genre) %>%
+  summarise(sum1 = sum(cwc1),sum2 = sum(cwc2),sum3 = sum(cwc3),sum4 = sum(cwc4),sum5 = sum(cwc5)) %>%
+  View()
+
+#Df for line chart
+
+#Main line (all genres combined)
+all_songs %>% 
   mutate(metric = numUnique/totalWords) %>%
-  group_by(genre, year) %>%
-  summarise(median_metric = median(metric)) %>%
-  ggplot(aes(x = year, y = median_metric, color = genre)) + 
-  geom_line() 
-
-raw_csv %>% 
   group_by(year) %>%
-  summarise(n = n()) %>%
-  ggplot(aes(x = year, y = n)) + 
-  geom_line() 
+  summarise(median_metric = median(metric)) -> main_line_df
 
+write_json(main_line_df, "main_line.json",dataframe = 'rows')
 
-raw_csv %>% 
+#Genre lines
+all_songs %>% 
   mutate(metric = numUnique/totalWords) %>%
-  group_by( year) %>%
-  summarise(median_metric = median(metric)) %>%
-  ggplot(aes(x = year, y = median_metric)) + 
-  geom_line() 
-
-
-raw_csv %>% 
   group_by(genre, year) %>%
-  summarise(n_songs = n(),
-            love_total = sum(love_words)/n_songs,
-            swear_total = sum(swear_words)/n_songs,
-            rock_total = sum(rock_words)/n_songs) %>%
-  ggplot(aes(x = genre, y = median_metric)) + 
-  geom_bar(stat = 'identity')
+  summarise(median_metric = median(metric)) -> genre_lines_df
+
+write_json(genre_lines_df, "genre_lines.json",dataframe = 'rows')
+
+
+all_songs %>%
+  group_by(year, genre) %>%
+  summarise( n = n(),
+             love_sum = sum(love_words), 
+             love_norm = love_sum/n,  
+             swear_sum = sum(swear_words),
+             swear_norm = swear_sum/n,
+             god_sum = sum(god_words),
+             god_norm = god_sum/n,
+             dance_sum = sum(dance_words),
+             dance_norm = dance_sum/n,
+             cool_sum = sum(cool_words),
+             cool_norm = cool_sum/n,
+             rock_sum = sum(rock_words),
+             rock_norm = rock_sum/n) -> word_freq_df
+
+write_json(word_freq_df, "word_freq.json",dataframe = 'rows')
+
+
+
+
